03694fddb337cc77237bce930427a0d44965b009
rtanguyen <rtanguyen@gmail.com>
2021-05-24 15:09:05 -0500

    updated constructor function to add employees

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+var employeePrompts = [
+    {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Enter manager office number',
+    },
+    {
+    type: 'input',
+    name: 'username',
+    message: "Enter engineer's GitHub username",
+    },
+    {
+    type: 'input',
+    name: 'school',
+    message: "Enter intern's school",
+    }]
 class team {
     constructor() {
         this.employee;
@@ -48,26 +64,8 @@ initializePrompts() {
     }
 });
 }
-    employeePrompts() {
-        // inquirer.prompt([
-    [{
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter manager office number',
-        },
-        {
-        type: 'input',
-        name: 'github',
-        message: "Enter engineer's GitHub username",
-        },
-        {
-        type: 'input',
-        name: 'school',
-        message: "Enter intern's school",
-        }]
-    }
-    
-    addManager() {
+
+    addManager(employeeData) {
         inquirer.prompt(employeePrompts[0])
         .then((office) => {
             let newManager = new Manager(
@@ -75,10 +73,41 @@ initializePrompts() {
                 employeeData.id,
                 employeeData.email,
                 office.officeNumber
-            )
-        });
+            );
+            console.log(newManager);
 
-        addEmployeesConfirm();
+            return this.addEmployeesConfirm();
+        });
+    }
+
+    addEngineer(employeeData) {
+        inquirer.prompt(employeePrompts[1])
+        .then((github) => {
+            let newEngineer = new Engineer(
+                employeeData.name,
+                employeeData.id,
+                employeeData.email,
+                github.username
+            );
+            console.log(newEngineer);
+
+            return this.addEmployeesConfirm();
+        });
+    }
+
+    addIntern(employeeData) {
+        inquirer.prompt(employeePrompts[2])
+        .then((school) => {
+            let newIntern = new Intern(
+                employeeData.name,
+                employeeData.id,
+                employeeData.email,
+                school.school
+            );
+            console.log(newIntern);
+
+            return this.addEmployeesConfirm();
+        });
     }
 
     addEmployeesConfirm() {
@@ -91,7 +120,7 @@ initializePrompts() {
             }
         ).then(({addConfirm}) => {
             if(addConfirm) {
-                initializePrompts();
+                this.initializePrompts();
             } else {
                 console.log('Your team profile has been generated!');
             }
